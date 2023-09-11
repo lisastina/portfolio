e2c35807dc2b06cbc5deb5425869ead524d0f087
Lisa Hansson <lisastina.hansson@gmail.com>
2023-09-11 21:08:01 +0200

    contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -3,7 +3,15 @@ import React from "react";
 const ContactPage = () => {
   return (
     <div className="contactPage">
-      <h1>contact</h1>
+      <div className="contactPage__text">
+        <h1>Hör av dig!</h1>
+        <a href="mailto: lisastina.hansson@gmail.com">
+          lisastina.hansson@gmail.com
+        </a>
+      </div>
+      <div className="contactPage__image">
+        <img src="/assets/letter-bird.png" alt="bird with letter" />
+      </div>
     </div>
   );
 };
