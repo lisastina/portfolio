a5ee93d73a941b75191581715002833af1c5d1dd
Lisa Hansson <lisastina.hansson@gmail.com>
2023-09-16 20:17:09 +0200

    firebase setup

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxBAfs0pyDzgtzwTV1cy6VgRsr7k569j0",
+  authDomain: "portfolio-9dda9.firebaseapp.com",
+  projectId: "portfolio-9dda9",
+  storageBucket: "portfolio-9dda9.appspot.com",
+  messagingSenderId: "471573855549",
+  appId: "1:471573855549:web:342eb863b1fe21b30bd954",
+};
+
+initializeApp(firebaseConfig);
