cfb8bc6f78f86d036cfe6a59a5d8ffe2ae017bee
Lisa Hansson <lisastina.hansson@gmail.com>
2023-10-06 12:35:19 +0200

    Buttons and images

--- src/components/EditProject.js
+++ src/components/EditProject.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const EditProject = () => {
   return (
-    <div className="projectPage editProject">
+    <form className="projectPage editProject">
       {/* First section */}
       <div className="projectPage__hero">
         <div className="projektPage__herotext">
@@ -32,10 +32,14 @@ const EditProject = () => {
             value="En fotobok om min hem-ö Hönö och dess historia. Äldre foton tagna av
             min pappa, min farfar och foton från det lokala museets arkiv
             blandas med mina egna för att jämföra dåtid med nutid."
+            placeholder="Skriv din text här..."
           ></textarea>
         </div>
         <div className="projectPage__heroimage">
           <img src="/assets/fotobok_01.jpg" alt="fotobok" />
+          <div className="add-remove">
+            <div>+</div>
+          </div>
         </div>
       </div>
 
@@ -50,9 +54,13 @@ const EditProject = () => {
           rows="6"
           value="Alla nya foton är tagna av mig, redigerade i Adobe Lightroom och
             Photoshop. Layout skapad i InDesign."
+          placeholder="Skriv din text här..."
         ></textarea>
         <div className="projectPage__image">
           <img src="/assets/fotobok_01.jpg" alt="fotobok" />
+          <div className="add-remove">
+            <div>+</div>
+          </div>
         </div>
 
         <textarea
@@ -64,12 +72,19 @@ const EditProject = () => {
           rows="4"
           value="Jag tryckte upp boken i liten upplaga för att dela med mig av till
             vänner och familj. Några få exemplar har sålts på Fiskemuseet på Hönö."
+          placeholder="Skriv din text här..."
         ></textarea>
         <div className="projectPage__image">
           <img src="/assets/fotobok_01.jpg" alt="fotobok" />
+          <div className="add-remove">
+            <div>+</div>
+          </div>
         </div>
         <div className="projectPage__image">
           <img src="/assets/fotobok_01.jpg" alt="fotobok" />
+          <div className="add-remove">
+            <div>+</div>
+          </div>
         </div>
         <textarea
           className="text"
@@ -82,12 +97,20 @@ const EditProject = () => {
             till platsen de tagits för att kunna jämföra sida vid sida
             förändringarna som skett under åren. Boken innehåller även texter och
             dikter lånade från lokala skribenter."
+          placeholder="Skriv din text här..."
         ></textarea>
         <div className="projectPage__image">
           <img src="/assets/fotobok_01.jpg" alt="fotobok" />
+          <div className="add-remove">
+            <div>+</div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="buttons">
+        <button className="deleteBtn">Radera projekt</button>
+        <input className="saveBtn" type="submit" value="Spara projekt" />
+      </div>
+    </form>
   );
 };
 export default EditProject;
